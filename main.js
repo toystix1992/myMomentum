e919d72abe343cd96bb96014f466ec5769c9edaa
toystix <toystix1992@gmail.com>
2020-12-05 21:18:38 +0300

    feat: add function get weather of your city

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const name = document.getElementById('name');
 const focus = document.getElementById('focus');
 const todayDate = document.getElementById('toDate');
 const bgButton = document.getElementById('bgButton');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
 
 // Function to show time
 const showTime = () => {
@@ -136,6 +140,28 @@ function setFocus(e) {
       localStorage.setItem('focus', e.target.innerText);
     }
   }
+
+  // Add weather on page
+  async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+  }
+
+  function setCity(event) {
+    if (event.code === 'Enter') {
+      getWeather();
+      city.blur();
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', getWeather);
+  city.addEventListener('keypress', setCity);
 
 name.addEventListener('keypress',setName);
 name.addEventListener('blur',setName);
